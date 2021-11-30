1d74da6113cef2aa00e7b1ab933722c39bf20448
Rodrigo <rodrigo.oliveira200310@gmail.com>
2021-11-29 21:58:06 -0300

    Creating timer inputs

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-var clock = document.getElementById("clock")
+// DOCUMENT ELEMENTS
 
+var clock = document.getElementById("clock")
+var clockScreen = document.getElementById("clock-screen")
+
+var timer = document.getElementById("timer")
+var alarm = document.getElementById("alarm")
+
+
+// FUNCTIONS
 setInterval(() => {
     clock.textContent = dayjs().format('HH:mm')
 }, 1000)
+
+timer.addEventListener("click", clickTimer)
+
+
+function clickTimer() {
+    let timerInput = document.createElement("input")
+    timerInput.type = "number"
+
+    let timerButton = document.createElement("button")
+    timerButton.textContent = "Confirm"
+
+    clockScreen.appendChild(timerInput)
+    clockScreen.appendChild(timerButton)
+
+    // let timerNumber = document.createElement("div")
+    // clock.appendChild(timerNumber)
+}
+
+
+
