6afcc8cfe7fb0a4551d6576d42010b832d74950a
Rodrigo <rodrigo.oliveira200310@gmail.com>
2021-11-30 21:19:02 -0300

    Library exchange

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
+
 // DOCUMENT ELEMENTS
 
 var clock = document.getElementById("clock")
 var clockScreen = document.getElementById("clock-screen")
 
 var timer = document.getElementById("timer")
+var countdown = document.getElementById("countdown")
 var alarm = document.getElementById("alarm")
 
 
 // FUNCTIONS
 setInterval(() => {
-    clock.textContent = dayjs().format('HH:mm')
+    clock.textContent = luxon.DateTime.now().toFormat("HH:mm")
 }, 1000)
 
-timer.addEventListener("click", clickTimer)
 
+countdown.addEventListener("click", clickCountdown)
 
-function clickTimer() {
+function clickCountdown() {
     let timerInput = document.createElement("input")
-    timerInput.type = "number"
+    timerInput.type = "time"
 
     let timerButton = document.createElement("button")
     timerButton.textContent = "Confirm"
+
+    // Click event of button
+    timerButton.addEventListener("click", () => {
+        // Get number input
+        let countdownValue = timerInput.value
+
+        // Remove input elements
+        clockScreen.removeChild(timerInput)
+        clockScreen.removeChild(timerButton)
+
+        // Add timer
+        let timerCountdown = document.createElement("div")
+        timerCountdown.textContent = countdown
+
+        setInterval(() => {
+            if (countdownValue != "00:00") {
+                let data = luxon.DateTime.fromFormat(countdownValue, "HH:mm")
+                data = data.minus({minutes: 1})
+                console.log(data.toFormat("HH:mm"))
+            }
+        }, 1000)
+    })
 
     clockScreen.appendChild(timerInput)
     clockScreen.appendChild(timerButton)
